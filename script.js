6d582a97b2d663b1c0906731d5ecb63fae972f83
Mottab766 <100230939+Mottab766@users.noreply.github.com>
2023-12-05 21:03:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const botaoCadastros = document.getElementById('btnCadastros');
+const formCadastros = document.getElementById('frm');
+const btnCancelar = document.getElementById ('btnCancelar');
+
+
+//deixa o form oculto ao acessar a aba
+formCadastros.style.display = 'none';
+
+//mostra o formulário e não tira
+botaoCadastros.addEventListener('click', () => {
+  formCadastros.style.display = 'flex';
+});
+
+//apenas tira o formulário
+btnCancelar.addEventListener('click', () => {
+  formCadastros.style.display = 'none';
+});
+
+
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
